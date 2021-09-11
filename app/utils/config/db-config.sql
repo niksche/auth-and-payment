a524213c7a95649d74b003fb61c0c9e047d1c7a8
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-09-11 18:52:46 +0300

    added config

--- app/utils/config/db-config.sql
+++ app/utils/config/db-config.sql
@@ -6,7 +6,7 @@ grant all privileges on database test1 to test3;
 
 create table users (user_id serial PRIMARY KEY, username varchar(50) UNIQUE NOT NULL, password varchar(60) not null);
 
-create table accounts (user_id serial PRIMARY KEY, username varchar(50) UNIQUE NOT NULL, balance real DEFAULT 8.0);
+create table accounts (user_id serial PRIMARY KEY, username varchar(50) UNIQUE NOT NULL, balance numeric DEFAULT 8.0, constraint check_balance_scale check (scale(balance) <= 1));
 
 GRANT ALL PRIVILEGES ON TABLE users TO test3;  
 GRANT ALL PRIVILEGES ON TABLE accounts TO test3;  
@@ -17,6 +17,4 @@ GRANT USAGE, SELECT ON SEQUENCE users_user_id_seq TO test3;
 GRANT USAGE, SELECT ON SEQUENCE accounts_user_id_seq TO test3; 
 
 
-
-
-`BEGIN; UPDATE accounts SET balance = balance - round(1.1 , 1) WHERE username = $1; COMMIT;`
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
